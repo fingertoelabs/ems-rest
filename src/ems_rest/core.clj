4658110bb2294364195fe7194c0fa9d74c57cf22
jreighley <joshua@sssonline.com>
2021-05-11 16:06:10 -0700

    War file working on IBMi

--- src/ems_rest/core.clj
+++ src/ems_rest/core.clj
@@ -3,7 +3,7 @@
 (defn handler [request]
   {:status 200
    :headers {"Content-Type" "text/plain"}
-   :body "Hello Ring! Am I reloadable?"})
+   :body "Hello Ring! Does the War file work on IBMi??"})
 
 (defn -main []
   "I don't do anything yet")
